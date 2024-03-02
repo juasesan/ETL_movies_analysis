4cc2ac4bb7368dd1861d6692046eaf3d716b5641
juasesan <jssa132@gmail.com>
2024-03-01 23:59:15 -0500

    Create function for querying the db and returning a df. Create EDA notebook

--- functions.py
+++ functions.py
@@ -1,6 +1,7 @@
 import requests
 import pandas as pd
 import matplotlib.pyplot as plt
+import sqlite3
 
 from airflow.providers.sqlite.hooks.sqlite import SqliteHook
 
@@ -88,3 +89,30 @@ def load_data_to_db(movies_df, genres_df, movies_genres):
     movies_df.to_sql(name='movies', con=engine, if_exists='append', index=False, method='multi')
     genres_df.to_sql(name='genres', con=engine, if_exists='append', index=False, method='multi')
     movies_genres.to_sql(name='movies_genres', con=engine, if_exists='append', index=False, method='multi')
+
+
+def query_sqlite_database(sql_query, database_path):
+    """
+    Executes an SQL query on a SQLite database and returns the results as a pandas DataFrame.
+
+    Parameters:
+    - sql_query: The SQL query string to be executed.
+    - database_path: The path to the SQLite database file.
+
+    Returns:
+    - A pandas DataFrame containing the results of the SQL query.
+    """
+
+    # Connect to the SQLite database
+    conn = sqlite3.connect(database_path)
+
+    try:
+        # Execute the query and return the results in a DataFrame
+        df = pd.read_sql_query(sql_query, conn)
+        return df
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return pd.DataFrame()  # Return an empty DataFrame in case of an error
+    finally:
+        # Ensure the database connection is closed
+        conn.close()
